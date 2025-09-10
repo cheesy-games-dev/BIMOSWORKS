ad96315b70e3fab452572ef477c70816c50d152a
Kaden <kadenzombie8@gmail.com>
2025-09-10 12:40:27 +0100

    Updated hips

--- Runtime/Rig/Animation/Hips.cs
+++ Runtime/Rig/Animation/Hips.cs
@@ -6,8 +6,12 @@ namespace KadenZombie8.BIMOS.Rig
     {
         private BIMOSRig _rig;
 
+        [SerializeField]
+        private AnimationCurve _hipsBackCurve = AnimationCurve.Linear(0f, 0f, 1f, 0.5f);
+
         private Transform _camera;
-        private Rigidbody _locomotionSphere;
+        private Rigidbody _locomotionSphereRigidbody;
+        private SphereCollider _locomotionSphereCollider;
 
         private float _spineLength;
         private Transform _headCameraOffset;
@@ -16,15 +20,20 @@ namespace KadenZombie8.BIMOS.Rig
         {
             _rig = BIMOSRig.Instance;
             _camera = _rig.ControllerRig.Transforms.Camera;
-            _locomotionSphere = _rig.PhysicsRig.Rigidbodies.LocomotionSphere;
+            _locomotionSphereRigidbody = _rig.PhysicsRig.Rigidbodies.LocomotionSphere;
+            _locomotionSphereCollider = _rig.PhysicsRig.Colliders.LocomotionSphere;
             _headCameraOffset = _rig.ControllerRig.Transforms.HeadCameraOffset;
             _spineLength = Vector3.Distance(_rig.AnimationRig.Transforms.Head.position, _rig.AnimationRig.Transforms.Hips.position);
         }
 
         void Update()
         {
-            float standingPercent = (_camera.position.y - _locomotionSphere.position.y + 0.2f) / _rig.AnimationRig.AvatarEyeHeight;
-            transform.position = _headCameraOffset.position + Vector3.down * _spineLength;
+            var floor = _locomotionSphereRigidbody.position.y + 0.2f;
+            var crouchingPercent = 1f - (_headCameraOffset.position.y - floor) / 1.1f;
+            var back = _hipsBackCurve.Evaluate(crouchingPercent);
+            var rotation = transform.rotation * Quaternion.Euler(back * 90f, 0f, 0f);
+            print(crouchingPercent);
+            transform.position = _headCameraOffset.position + rotation * (Vector3.down * _spineLength);
         }
     }
 }
